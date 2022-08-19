ec7fe9dffeae89da7c4dc22318c54e9c4c2f136b
Nishan Ghimire <111546233+marktraces@users.noreply.github.com>
2022-08-19 06:55:27 +0545

    Add files via upload

---
+++ Integers and Floats.py
@@ -0,0 +1,24 @@
+from cmath import log
+import imp
+import math
+
+#Basic Arithmetic and Logarithm
+
+def calcnlog(a,b):
+    print ('\n 1. Sum of the two numbers is ' + str(a + b) + '.')
+    print (' 2. Difference of the two numbers is ' + str(a - b) + '.')
+    print (' 3. The product of the two numbers is ' + str(a*b) + '.')
+    print (' 4. When the first number is divided by the second one, we get ' + str (a/b) + '.')
+    print (' 5. A to the power of b is ' + str(a**b) + '.')
+    print (' 6. The logarithm of your num is ' + str(math.log(b,a)) + '.\n')
+
+
+#a = float(input("\n Enter your number :\n ---> \t"))
+#b = float(input("Enter your number :\n ---> \t"))
+#calcnlog(a,b)
+
+
+#The Zen Of Python
+#import this
+
+
---
+++ String Manipulation.py
@@ -0,0 +1,23 @@
+#Upper, Lower and Title
+
+message = "this is a message that is written by me, 'Nishan Ghimire'\n"
+a = message.title()
+b = message.upper()
+c = b.lower()
+print(a + b + c )
+
+#Stripping
+
+Strip_message = "   This is Nishan Ghimire!  \n \t "
+d = Strip_message.strip()
+e = Strip_message.rstrip()
+f = Strip_message.lstrip()
+print (d)
+print (e)
+print (f)
+
+#Quote Management
+
+quote = "'I have a total of 4 crushes.' \n \t  - Sameeksha Sharma \n"
+print (quote)
+
